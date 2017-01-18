7d4569b629c887273267eb069094289106b218d0
Saket Vatsa <Saket Vatsa>
2017-01-18 17:22:21 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var printBacon=function () {
+	console.log("bacon is healthy");
+	// body...
+}
+printBacon();
+setTimeout(printBacon,3000);
---
+++ js/miniproject.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$(".i").click(function(){
+		$(this).addClass('myClass');
+	});
+});
